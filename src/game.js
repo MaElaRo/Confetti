da6a4acde99653e2033519c6e032b1856e53df6e
MaElaRo <50324006+MaElaRo@users.noreply.github.com>
2019-06-17 23:06:27 +0200

    Update game.js

--- src/game.js
+++ src/game.js
@@ -4,10 +4,7 @@ class Food {
     this.y = random(0, 600);
     //this.speed = 3;
   }
-  /*move() {
-    this.x += random(-this.speed, this.speed);
-    this.y += random(-this.speed, this.speed);
-  }*/
+  
   draw() {
     this.foodObj = ellipse(this.x, this.y, 30, 30);
   }
